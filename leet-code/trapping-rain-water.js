7bf9495c93c221b65bfe6d0301388c72a5119ddf
huseyin koclar <mhuseyin.koclar@gmail.com>
2022-09-10 18:21:17 +0300

    new solution

--- leet-code/trapping-rain-water.js
+++ leet-code/trapping-rain-water.js
@@ -5,14 +5,15 @@
  var trap = function(height) {
     var start = findStart(height);
     var end = findEnd(height);
-    var rain = findRain(start, end, height);
+    height = minimizeMap(start, end, height);
+    
+    return calculate(height);
 };
 
 var findStart = function(height){
     var number;
     for(var i = 0; i<height.length; i++){
         if(height[i] > 0 && height[i+1] < height[i]){
-            console.log("start: " + i);
             number = i;
             break;
         }
@@ -23,7 +24,6 @@ var findStart = function(height){
 var findEnd = function(height){
     for(var i = height.length-1; i >= 0; i--){
         if(height[i] > 0 && height[i-1] < height[i]){
-            console.log("end: " + i);
             number = i;
             break;
         }
@@ -31,20 +31,45 @@ var findEnd = function(height){
     return number
 }
 
-var findRain = function(start, end, height){
-    var arr = height.slice(start, end+1);
-    var max = Math.max(...arr);
-    var maxIndex = arr.indexOf(max);
-    var left = arr.slice(0, maxIndex);
-    var right = arr.slice(maxIndex+1, arr.length+1);
-    console.log(left)
-    console.log(right)
-
-    
-    return number
+var minimizeMap = function(start, end, height){
+    var map = height.slice(start, end+1);
+    return map;
 }
 
 
+var calculate = function(map){
+    if(map == "") return 0;
+    var rain = 0;
+    var max = Math.max(...map);
+    var maxIndex = map.indexOf(max);
+    var secondMaxIndex = map.indexOf(nextBiggest(map));
+    var secondMax = map[secondMaxIndex];
+    if(maxIndex == secondMaxIndex) secondMaxIndex = map.indexOf(secondMax, maxIndex+1);
+    var tempArr;
+    if(maxIndex > secondMaxIndex){
+        tempArr = map.slice(secondMaxIndex, maxIndex);
+        map = map.slice(0, secondMaxIndex+1).concat(map.slice(maxIndex+1, map.length));
+    }
+    else {
+        tempArr = map.slice(maxIndex+1, secondMaxIndex+1);
+        map = map.slice(0, maxIndex+1).concat(map.slice(secondMaxIndex+1, map.length));
+    }
+    for(var i = 0; i<tempArr.length; i++){
+        rain += secondMax - tempArr[i];
+    }
+    return rain + calculate(map);
+}
+//nextbiggerda hata var en büyük 2. sayıyı buluyor en büyük sayıdan 2 tane varsa onu bulması gerekli
+function nextBiggest(arr) {
+    var max = Math.max(...arr);
+    var maxIndex = arr.indexOf(max);
+    //new array without max
+    var newArr = arr.slice(0, maxIndex).concat(arr.slice(maxIndex+1, arr.length));
+    var secondMax = Math.max(...newArr);
+    return secondMax;
+  }
+
+var rain = 0;
 
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 console.log(trap(height));
