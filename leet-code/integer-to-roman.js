7b70e18b1a930a0e44d1b0d9c3a9b7baafb64175
Hüseyin Koçlar <mhuseyin.koclar@gmail.com>
2023-08-02 12:56:04 +0300

    new solution

--- leet-code/integer-to-roman.js
+++ leet-code/integer-to-roman.js
@@ -6,20 +6,7 @@ function intToRoman(num) {
     if (num <= 0 || num >= 4000) {
       throw new Error('Number out of range (1-3999)');
     }
-    const romanNumerals = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1
+    const romanNumerals = {M: 1000,CM: 900,D: 500,CD: 400,C: 100,XC: 90,L: 50,XL: 40,X: 10,IX: 9,V: 5,IV: 4,I: 1
     };
     let result = '';
     for (const romanNumeral in romanNumerals) {
